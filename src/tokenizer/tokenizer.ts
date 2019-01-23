MikeHopcroft/TokenFlow

commit 0cc15a1fd78655170887013cb2cf16e717bb642e

Tokenizer.aliases.

src/tokenizer/tokenizer.ts
@@ -1,14 +1,22 @@
 import { newStemmer, Stemmer as SnowballStemmer } from 'snowball-stemmers';
 import { v3 } from 'murmurhash';
 
-import { Edge, findBestPath } from './best_path';
-import { DynamicGraph, Graph } from '../graph';
-import { levenshtein } from '../matchers';
+import { Edge, DynamicGraph, Graph } from '../graph';
+import { DownstreamTermPredicate, levenshtein, Matcher } from '../matchers';
 import { Token, TokenFactory } from './tokens';
 import { HASH, ID, PID } from './types';
 import { Logger } from '../utilities';
 
 export type StemmerFunction = (term: string) => string;
+
+export interface Alias {
+    pid: PID;
+    text: string;
+    stemmed: string;
+    hashes: number[];
+    matcher: Matcher;
+    isDownstream: DownstreamTermPredicate<number>;
+}
 
 export class Tokenizer {
     debugMode = true;
@@ -22,11 +30,8 @@ export class Tokenizer {
     // Murmurhash seed.
     seed = 0;
 
-    items: string[] = [];
-    pids: PID[] = [];
-
-    hashedItems: number[][] = [];
-    stemmedItems: string[] = [];
+    // Information about each alias.
+    aliases: Alias[] = [];
 
     hashToText: { [hash: number]: string } = {};
     hashToFrequency: { [hash: number]: number } = {};
@@ -144,7 +149,7 @@ export class Tokenizer {
     }
 
     decodeEdge = (edge: Edge) => {
-        return `Edge("${this.items[edge.label]}", score=${edge.score}, length=${edge.length})`;
+        return `Edge("${this.aliases[edge.label].text}", score=${edge.score}, length=${edge.length})`;
     }
 
     pidToName = (pid: PID) => {
@@ -178,7 +183,7 @@ export class Tokenizer {
             // TODO: EXPERIMENT 1: filter out downstream words.
             else {
                 // TODO: Where does toUpperCase and replacing spaces with underscores go?
-                const name = pidToName(this.pids[edge.label]);
+                const name = pidToName(this.aliases[edge.label].pid);
                 const text = `[${name}]`;
                 rewritten.push(text);
                 termIndex += edge.length;
@@ -196,7 +201,7 @@ export class Tokenizer {
             }
             else {
                 const children = tokens.slice(termIndex, termIndex + edge.length);
-                output.push(tokenFactory(this.pids[edge.label], children));
+                output.push(tokenFactory(this.aliases[edge.label].pid, children));
                 termIndex += edge.length;
             }
         }
@@ -215,18 +220,23 @@ export class Tokenizer {
         // Internal id for this item. NOTE that the internal id is different
         // from the pid. The items "manual transmission" and "four on the floor"
         // share a pid, but have different ids.
-        const id = this.items.length;
-        this.items.push(text);
-        this.pids.push(pid);
+        const id = this.aliases.length;
 
         // Split input string into individual terms.
         const terms = text.split(/\s+/);
 
         const stemmed = terms.map(this.stemTermInternal);
-        this.stemmedItems.push(stemmed.join(' '));
 
         const hashed = stemmed.map(this.hashTerm);
-        this.hashedItems.push(hashed);
+
+        this.aliases.push({
+            pid,
+            text,
+            stemmed: stemmed.join(' '),
+            hashes: hashed,
+            matcher: levenshtein,
+            isDownstream: this.isDownstreamTerm
+        });
 
         hashed.forEach((hash, index) => {
             // Add this term to hash_to_text so that we can decode hashes later.
@@ -234,6 +244,8 @@ export class Tokenizer {
                 this.hashToText[hash] = stemmed[index];
             }
 
+            // TODO: Consider removing code to compute frequencies.
+            // Currently this code is unused. Would it be a good signal?
             // Update term frequency
             // DESIGN ALTERNATIVE: could use lengths of posting lists instead.
             if (hash in this.hashToFrequency) {
@@ -459,7 +471,7 @@ export class Tokenizer {
                 // the tail of the query.
                 const tail = hashed.slice(index);
                 const scored = items.map((item) =>
-                    ({ ...this.score(tail, this.hashedItems[item]), label: item }));
+                    ({ ...this.score(tail, this.aliases[item].hashes), label: item }));
 
                 const sorted = scored.sort((a, b) => b.score - a.score);
 

samples/graph_demo.ts
@@ -1,335 +1,0 @@
-import { Edge, Graph } from '../src';
-
-interface Path2 {
-    edges: Edge[];
-    score: number;
-}
-
-function startsWith<T>(path: T[], prefix: T[]): boolean {
-    if (prefix.length <= path.length) {
-        for (const [index, value] of prefix.entries()) {
-            if (path[index] !== value) {
-                break;
-            }
-        }
-        return true;
-    }
-    return false;
-}
-
-function validPath(path: Edge[]): boolean {
-    for (const edge of path) {
-        if (edge.discarded) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function pathsEqual(a: Edge[], b: Edge[]): boolean {
-    if (a.length === b.length) {
-        for (const [index, value] of a.entries()) {
-            if (value !== b[index]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    return false;
-}
-
-class Graph3 {
-    paths: Path2[] = [];
-
-    left: Edge[] = [];
-    right: Edge[] = [];
-    current = 0;
-
-    constructor(graph: Edge[][]) {
-        const defaultEdge = { score: 0, length: 1, label: -1 };
-        const augmented = graph.map((edges: Edge[]) => [
-            defaultEdge, ...edges
-        ]);
-        this.createPaths([], augmented);
-        this.paths.sort((a, b) => b.score - a.score);
-
-        const path = this.nextBestPath(this.left);
-        if (path === null) {
-            throw TypeError('Graph3.constructor(): cannot find initial path.');
-        }
-        this.right = path.edges;
-      
-        for (const path of this.paths) {
-            printPath(path.edges);
-        }
-
-        console.log('----------------');
-        printPath(this.left);
-    }
-
-    createPaths(prefix: Edge[], graph: Edge[][]) {
-        let vertex = 0;
-        for (const edge of prefix) {
-            vertex += edge.length;
-        }
-
-        if (vertex < graph.length) {
-            for (const edge of graph[vertex]) {
-
-                // TODO: consider case where edge extends beyond last vertex.
-
-                const edges = [...prefix, edge];
-                let score = 0;
-                for (const e of edges) {
-                    score += e.score;
-                }
-
-                // TODO: this is storing a path prefix. Limit to complete paths?
-                if (vertex + edge.length === graph.length) {
-                    this.paths.push({ edges, score });
-                }
-                else {
-                    this.createPaths(edges, graph);
-                }
-            }
-        }
-    }
-
-    nextBestPath(prefix: Edge[]): Path2 | null {
-        let index;
-        for (index = 0; index < this.paths.length; ++index) {
-            if (startsWith(this.paths[index].edges, prefix) &&
-                validPath(this.paths[index].edges)) {
-                return this.paths[index];
-            }
-        }
-
-        return null;
-    }
-
-    // advance(): boolean {
-    //     const edge = this.right.shift();
-    //     if (edge !== undefined) {
-    //         this.current += edge.length;
-    //         this.left.push(edge);
-    //         return true;
-    //     }
-    //     return false;
-    // }
-
-    // retreat(reset: boolean) {
-    //     // Ignore `reset`.
-
-    //     const edge = this.left.pop();
-        
-    //     if (edge === undefined) {
-    //         throw TypeError('Graph3.retreat(): attempt to retreat from first vertex.');
-    //     }
-    //     else {
-    //         this.current -= edge.length;
-    //         this.right.unshift(edge);
-    //     }
-    // }
-
-    // discard(): boolean {
-    //     if (this.right.length === 0) {
-    //         throw TypeError('Graph3.discard(): attempt to discard from last vertex.');   
-    //     }
-
-    //     this.right[0].discarded = true;
-    //     this.right = this.findPath2(this.current);
-    //     return this.right.length > 0;
-
-
-    //     const path = this.nextBestPath(this.left);
-    //     if (path !== null) {
-    //         const edge = path.edges.shift();
-    //         if (edge) {
-    //             this.left.push(edge);
-    //         }
-    //         return true;
-    //     }
-    //     return false;
-    // }
-}
-
-function printPath(path: Edge[]) {
-    let score = 0;
-    const vertices = [0];
-    for (const edge of path) {
-        vertices.push(vertices[vertices.length - 1] + edge.length);
-        score += edge.score;
-    }
-    console.log(`  ${vertices.map(n => String.fromCharCode(97 + n)).join('')}: ${score}`);
-}
-
-function getPath(g: Graph) {
-    const path = [ ...g.left, ...g.right ];
-    const current = g.current;
-    let score = 0;
-    const vertices = [0];
-    let text = (current === 0) ? 'a*' : 'a';
-    for (const edge of path) {
-        const vertex = vertices[vertices.length - 1] + edge.length;
-        vertices.push(vertex);
-        text = text.concat(String.fromCharCode(97 + vertex));
-        if (current === vertices[vertices.length - 1]) {
-            text = text.concat('*');
-        }
-        score += edge.score;
-    }
-    return `${text}: ${score}`;
-}
-
-function scorePath(path: Edge[]): number {
-    let score = 0;
-    const vertices = [0];
-    for (const edge of path) {
-        score += edge.score;
-    }
-    return score;
-}
-
-function check(g: Graph, g3: Graph3) {
-    const prefix = g.left.slice(0, -1);
-    const path = [...g.left, ...g.right];
-    const path3 = g3.nextBestPath(prefix);
-    printPath(path);
-
-    if (path3) {
-        printPath(path3.edges);
-        if (pathsEqual(path, path3.edges)) {
-            console.log('  Paths equal');
-        }
-        else {
-            if (scorePath(path) === path3.score) {
-                console.log('  Different paths - same scores');
-            }
-            else {
-                console.log('  Different paths - different scores');
-            }
-        }
-    }
-    else {
-        console.log('  Path is null');
-    }
-}
-
-let level = 0;
-let counter = 0;
-
-
-function walk(g: Graph): string[] {
-    let paths: string[] = [];
-    ++level;
-    const indent = ' '.repeat(level *2);
-
-    while (true) {
-        g.advance();
-        console.log(`${indent}advance() to ${getPath(g)}`);
-
-        if (g.complete()) {
-            ++counter;
-            console.log(`${indent}############### ${counter}: ${getPath(g)}`);
-            paths.push(`${indent}${counter}: ${getPath(g)}`);
-        }
-        else {
-            console.log(`${indent}walk()`);
-            paths = paths.concat(walk(g));
-        }
-
-        g.retreat(true);
-        {
-            const x = getPath(g);
-            if (x === 'ad*eg: 4.952') {
-            // if (x === 'ade*fg: 2.992') {
-                console.log(`x = ${x}`);
-                console.log('stop');
-            }
-        }
-        console.log(`${indent}retreat() to ${getPath(g)}`);
-    
-        if (!g.discard()) {
-            console.log(`${indent}discard() to defaultEdge ${getPath(g)}`);
-            break;
-        }
-        else {
-            console.log(`${indent}discard() to ${getPath(g)}`);
-        }
-    }
-
-    --level;
-    return paths;
-}
-
-function go() {
-    const edgeList: Edge[][] = [];
-    for (let i = 0; i < 6; ++i) {
-        const edges: Edge[] = [];
-        for (let j = 2; i + j <= 6; ++j) {
-            const label = i * 10 + i + j;
-            const length = j;
-            const score = j - Math.pow(0.2, j);
-            console.log(`label=${label}, length=${length}, score=${score} avg=${score/length}`);
-            edges.push({ score, length, label });
-        }
-        edgeList.push(edges);
-    }
-
-    const graph = new Graph(edgeList);
-
-    console.log('======================');
-    const paths = walk(graph);
-
-    console.log('xxx=================');
-    for (const path of paths) {
-        console.log(path);
-    }
-    console.log('yyy================');
-
-    const graph3 = new Graph3(edgeList);
-
-    console.log('=================');
-
-    // console.log('advance()');
-    // graph.advance();
-    // check(graph, graph3);
-
-    // console.log('discard()');
-    // graph.discard();
-    // check(graph, graph3);
-
-    // console.log('discard()');
-    // graph.discard();
-    // check(graph, graph3);
-
-    // console.log('discard()');
-    // graph.discard();
-    // check(graph, graph3);
-
-    // console.log('discard()');
-    // graph.discard();
-    // check(graph, graph3);
-
-    // console.log('discard()');
-    // graph.discard();
-    // check(graph, graph3);
-
-    // console.log('discard()');
-    // graph.discard();
-    // check(graph, graph3);
-
-
-
-    // console.log('retreat(false)');
-    // graph.retreat(false);
-    // graph.advance();
-    // printBestPath(graph);
-
-    // console.log('retreat(true) - BUG: not implemented');
-    // graph.retreat(true);
-    // graph.advance();
-    // printBestPath(graph);
-}
-
-go();

src/index.ts
@@ -1,5 +1,4 @@
-// Export Graph here once the version in best_path.ts has been removed.
-// export * from './graph';
+export * from './graph';
 export * from './ingestors';
 export * from './recognizers';
 export * from './relevance_suite';

src/matchers/common.ts
@@ -6,6 +6,14 @@ export function GenericEquality<T>(a: T, b: T): boolean {
     return a === b;
 }
 
+export type Matcher = (
+    query: number[],
+    prefix: number[],
+    isDownstreamTerm: DownstreamTermPredicate<number>,
+    isTokenHash: TokenPredicate<number>,
+    isEqual: EqualityPredicate<number>
+) => DiffResults<number>;
+
 export interface DiffResults<T> {
     match: T[];             // The sequence that represents the match that
                             // minimizes Levenshtein edit distance.

src/tokenizer/best_path.ts
@@ -1,231 +1,0 @@
-export interface Edge {
-    score: number;
-    length: number;
-    label: number;
-    // TODO: do we really want to expose the discarded property to the caller?
-    discarded?: boolean;
-}
-
-export class Vertex {
-    edges: Edge[];
-    score = -Infinity;
-    backtraceVertex: Vertex | null = null;
-    backtraceEdge: Edge | null = null;
-    checkpoint = false;
-
-    constructor(edges: Edge[], score: number) {
-        this.edges = edges;
-        this.score = score;
-    }
-}
-
-export class Graph {
-    vertices: Vertex[];
-
-    defaultEdge: Edge;
-    left: Edge[] = [];
-    right: Edge[] = [];
-    current = 0;
-
-    constructor(edgeLists: Edge[][]) {
-        // TODO: ISSUE: should edgelists be sorted?
-        // Probably not necessary as we always pick the edge
-        // on the best scoring path.
-
-        // TODO: clear the discarded property on each edge?
-        // Or make a copy of edge lists? Don't really like
-        // side-effecting caller's edges, but copying them
-        // seems wasteful.
-        // TODO: provide a method for user to clear discarded property?
-        const vertexCount = edgeLists.length;
-
-        // NOTE: using label value of -1 as sentinel for no label.
-        this.defaultEdge = { score: 0, length: 1, label: -1 };
-        this.vertices = edgeLists.map((edges, index) => {
-            const score = index === 0 ? 0 : -Infinity;
-            return new Vertex([{ ...this.defaultEdge}, ...edges], score);
-            // return new Vertex([this.defaultEdge, ...edges], score);
-        });
-        this.vertices.push(new Vertex([], -Infinity));
-
-        // TODO: existing token-flow code will find the best
-        // path twice. Modify code below to return this.left.
-        this.right = this.findPath2(0);
-    }
-
-    // Returns true when the current path extends to the final vertex.
-    complete(): boolean {
-        return this.current === this.vertices.length - 1;
-    }
-
-    // Attempt to extend the current path by advancing forward along the next
-    // Edge in the top-scoring path through the current vertex. Returns the
-    // extended path. NOTE that calling advance() from the last vertex is legal,
-    // but the path will not be extended. At all other vertices, advance() will
-    // succeed in extending the path because of default edges which cannot be
-    // discarded.
-    advance(): boolean {
-        // TODO: consider throwing if this.complete() === true.
-        const edge = this.right.shift();
-        if (edge !== undefined) {
-            this.current += edge.length;
-            this.left.push(edge);
-            return true;
-        }
-        return false;
-    }
-
-    // Attempt to move backwards one edge to the previous vertex. Before
-    // moving, clear the checkpoint on the current vertex. When `reset` is
-    // true, reinstate edges discarded from the previous vertex.
-    retreat(reset: boolean): Edge[] {
-        this.retreatHelper(reset);
-//        this.right = this.findPath2(this.current);
-        return this.left;
-    }
-    
-    private retreatHelper(reset: boolean) {
-        const edge = this.left.pop();
-        
-        if (edge === undefined) {
-            throw TypeError('Graph.retreatHelper(): attempt to retreat from first vertex.');
-        }
-        else {
-            this.vertices[this.current].checkpoint = false;
-            for (const e of this.vertices[this.current].edges) {
-                e.discarded = false;
-            }
-            this.current -= edge.length;
-
-            // TODO: following line shouldn't be necessary.
-            // FOLLOWUP: necessary now because retreat no longer recomputes path.
-            this.right.unshift(edge);
-        }
-    }
-
-    // Attempts to remove the most recently traversed Edge from the graph
-    // and then advances from the previous vertex along the new top-scoring
-    // path. Returns the new Edge.
-    //
-    // NOTE: this method always returns an edge because
-    //   1. discard() can never retreat to the last vertex as this would
-    //      imply a vertex after the last vertex.
-    //   2. discard() is not allowed to remove the default edge.
-    //
-    // NOTE: edges are not necessarily removed in order of decreasing score.
-    // Need to mark edges as removed.
-    discard(): boolean {
-        if (this.right.length === 0) {
-            throw TypeError('Graph.discard(): attempt to discard from last vertex.');   
-        }
-
-        this.right[0].discarded = true;
-        this.right = this.findPath2(this.current);
-        return this.right.length > 0;
-    }
-
-
-    // Marks the current vertex as checkpointed for possible later use by the
-    // restore() method.
-    checkpoint() {
-        this.vertices[this.left.length].checkpoint = true;
-    }
-
-    // Retreats to the most recently checkpointed vertex, removing checkpoint
-    // markers along the way.
-    // NOTE: Attempting to restore from an empty stack will result in an
-    // exception.
-    restore(reset: boolean): Edge[] {
-        if (this.left.length > 0) {
-            while (this.left.length !== 0) {
-                this.retreatHelper(reset);
-
-                // TODO: BUGBUG: current is wrong. Need vertex index, not edge number.
-                if (this.vertices[this.left.length].checkpoint) {
-                    break;
-                }
-            }
-//            this.right = this.findPath2(this.left.length);
-        }
-        return this.left;
-    }
-
-    // Finds the highest scoring path from this.vertices[start] that does
-    // not pass through a discarded edge.
-    private findPath2(start: number): Edge[]
-    {
-        // Initialize vertices and forward propagate paths.
-        for (let index = start; index < this.vertices.length; ++index) {
-            const vertex = this.vertices[index];
-            vertex.score = index === start ? 0 : -Infinity;
-            vertex.backtraceVertex = null;
-            vertex.backtraceEdge = null;
-        }
-
-        for (let index = start; index < this.vertices.length; ++index) {
-            const vertex = this.vertices[index];
-            for (const edge of vertex.edges) {
-                if (edge.discarded !== true) {
-                    const targetIndex = index + edge.length;
-
-                    if (targetIndex < this.vertices.length) {
-                        const target = this.vertices[targetIndex];
-                        const newScore = vertex.score + edge.score;
-                        if (target.score < newScore) {
-                            target.score = newScore;
-                            target.backtraceVertex = vertex;
-                            target.backtraceEdge = edge;
-                        }
-                    }
-                }
-            }
-        }
-
-        // Extract path by walking backwards from last vertex.
-        const reversePath = [];
-        let current = this.vertices[this.vertices.length - 1];
-        while (current.backtraceVertex) {
-            reversePath.push(current.backtraceEdge as Edge);
-            current = current.backtraceVertex;
-        }
-
-        const forwardPath = reversePath.reverse();
-
-        return forwardPath;
-    }
-
-    findPath() {
-        // Forward propagate paths.
-        this.vertices.forEach((vertex, index) => {
-            vertex.edges.forEach((edge) => {
-                const targetIndex = index + edge.length;
-                if (targetIndex < this.vertices.length) {
-                    const target = this.vertices[targetIndex];
-                    const newScore = vertex.score + edge.score;
-                    if (target.score < newScore) {
-                        target.score = newScore;
-                        target.backtraceVertex = vertex;
-                        target.backtraceEdge = edge;
-                    }   
-                }
-            });
-        });
-
-        // Extract path by walking backwards from last vertex.
-        const reversePath = [];
-        let current = this.vertices[this.vertices.length - 1];
-        while (current.backtraceVertex) {
-            reversePath.push(current.backtraceEdge as Edge);
-            current = current.backtraceVertex;
-        }
-
-        const forwardPath = reversePath.reverse();
-
-        return forwardPath;
-    }
-}
-
-export function findBestPath(edgeLists: Edge[][]) {
-    const graph = new Graph(edgeLists);
-    return graph.findPath();
-}

src/tokenizer/index.ts
@@ -1,5 +1,4 @@
 export * from './alias_generator';
-export * from './best_path';
 export * from './item_collection';
 export * from './pattern_recognizer';
 export * from './recognizer';

test/tokenizer/tokenizer.test.ts
@@ -17,9 +17,9 @@ describe('Tokenizer', () => {
                 const pid = item[0];
                 const text = item[1];
                 tokenizer.addItem(pid, text, false);
-                assert.equal(tokenizer.items.length, index + 1);
-                assert.equal(tokenizer.items[index], text);
-                assert.equal(tokenizer.pids[index], pid);
+                assert.equal(tokenizer.aliases.length, index + 1);
+                assert.equal(tokenizer.aliases[index].text, text);
+                assert.equal(tokenizer.aliases[index].pid, pid);
             });
         });
 
@@ -28,7 +28,7 @@ describe('Tokenizer', () => {
             const tokenizer = new Tokenizer(downstreamWords, undefined, true, false);
             const input = 'small unsweeten ice tea';
             tokenizer.addItem(1, input, false);
-            const observed = tokenizer.hashedItems[0];
+            const observed = tokenizer.aliases[0].hashes;
             const expected:number[] = [2557986934, 1506511588, 4077993285, 1955911164];
             assert.deepEqual(observed, expected);
         });
@@ -46,8 +46,8 @@ describe('Tokenizer', () => {
     
             // Verify that item text and stemmed item text are recorded.
             items.forEach((item, index) => {
-                assert.equal(tokenizer.items[index], items[index]);
-                assert.equal(tokenizer.stemmedItems[index], items[index]);
+                assert.equal(tokenizer.aliases[index].text, items[index]);
+                assert.equal(tokenizer.aliases[index].stemmed, items[index]);
             });
 
             // Verify that posting lists are correct.

commit 79cc1332ff9b5052942b6677dc85fcaf447425b0

Fixed crash in Tokenizer.decodeEdge() introduced in 0cc15a1.

src/tokenizer/tokenizer.ts
@@ -144,7 +144,9 @@ export class Tokenizer {
     }
 
     decodeEdge = (edge: Edge) => {
-        return `Edge("${this.aliases[edge.label].text}", score=${edge.score}, length=${edge.length})`;
+        const alias = this.aliases[edge.label];
+        const text = (alias)?alias.text:'unknown';
+        return `Edge("${text}", score=${edge.score}, length=${edge.length})`;
     }
 
     pidToName = (pid: PID) => {